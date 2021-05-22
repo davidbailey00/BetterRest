035528043ed6c69c54107e945e5bec3b877a1d9e
David Bailey <davidbailey00@outlook.com>
2021-05-22 09:41:49 +0100

    initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by David Bailey on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
